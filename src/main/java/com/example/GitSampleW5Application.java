04306538b27d0bc919e980f2d6dc497d5b62a0a0
MotokiIzawa <motoki.izawa@rakus-partners.co.jp>
2021-07-29 14:31:11 +0900

    first commit

---
+++ src/main/java/com/example/GitSampleW5Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleW5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleW5Application.class, args);
+	}
+
+}
